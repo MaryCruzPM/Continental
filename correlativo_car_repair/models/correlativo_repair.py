41a66896ec8496c7c76651962e3d62cbb45eff30
MarycruzPM <pam9553asmh@gmail.com>
2019-07-11 16:56:59 -0500

    [ADD]correlativo de modulo de reparación

---
+++ correlativo_car_repair/models/correlativo_repair.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, api, _
+from datetime import date, time, datetime
+from odoo import tools
+from odoo.exceptions import UserError, ValidationError
+
+
+class correlativo_repair(models.Model):
+    _inherit = 'fleet.repair'
+    #valor= fields.Char(compute='default_get')
+    #count_fleet_repair  = fields.Integer(string='Repair Orders', compute='_compute_repair_id')
+    
+    @api.model
+    def default_get(self,fields):
+        #res = super(correlativo_repair, self).default_get(fields)
+        #res['sequence'] = self.env['ir.sequence'].get('fleet.repair')
+        #res['receipt_date'] = datetime.now()
+        #if self.fleet_id:
+        #	res['sequence'] = self.env['ir.sequence'].get('fleet.repair')
+        	#res['receipt_date'] = datetime.now()
+        #print(res)
+        return fields
