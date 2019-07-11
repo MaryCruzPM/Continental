f13badcb4d20dc5deec16a0271d2005c14543e46
MarycruzPM <pam9553asmh@gmail.com>
2019-07-11 16:16:51 -0500

    [MOD]modificacón de campos default, se termino tanto factura y ventas

--- campos_default/models/order_account_invoice.py
+++ campos_default/models/order_account_invoice.py
@@ -3,9 +3,8 @@ from odoo import fields
 from odoo import models
 from odoo import api
 # modelo de campos a editar desde factura borrados
-class editable_factura(models.Model):
+class factura(models.Model):
 	_inherit='account.invoice'
-
 
 	x_vehiculo = fields.Many2one('fleet.vehicle',string="Vehiculo")
 	x_marca = fields.Many2one('fleet.vehicle.model.brand')
@@ -13,37 +12,45 @@ class editable_factura(models.Model):
 	x_Matricula = fields.Char()
 	x_year_modelo = fields.Char()
 	x_unidad =fields.Char()
+	x_kilometraje=fields.Float()
+	x_unidadKilometro=fields.Selection(selection=[('kilometers','Kilómetros'),('Miles','Millas')],)
 
-
-
-
-
-	# create_form_fleet = fields.Boolean(string='Fleet')
-    
- #    @api.model
- #    def create(self, vals):
- #        if vals.get('origin'):
- #            sale_obj = self.env['sale.order'].search([('name', '=', vals.get('origin'))])
- #            if sale_obj  and sale_obj.workorder_id and sale_obj.workorder_id.fleet_repair_id:
- #                vals.update({'create_form_fleet': True})
- #        return super(AccountInvoice, self).create(vals= vals)
-
-
-
+# 	@api.model_create_multi
+# 	def create(self,vals):
+# 		print("dentro de unidad")
+# 		#if vals.get('origin'):
+# 		sale_obj = self.env['sale.order'].search([('name', '=', vals.get('origin'))])
+# 		print(sale_obj.fleet_repair_id.fleet_id.id)
+# 		print(sale_obj)
+# 		print(sale_obj.name)
+# 		res = super(factura, self).create(vals)
+# 		print(res)
+# 		print(res.id)
+# 		repair_x=self.env['fleet.repair'].search([('id','=',sale_obj.fleet_repair_id.id)],limit=1)
+# 		print(repair_x)
+# 		print(repair_x.fleet_repair_line.fleet_id.model_id)
+# 		#res.write({'x_modelo':[(4,repair_x.fleet_repair_line.fleet_id.model_id)]})
+# 		res.x_modelo=repair_x.fleet_repair_line.fleet_id.model_id
+# 		res.x_marca=repair_x.fleet_repair_line.fleet_id.brand_id
+# 		res.x_Matricula=repair_x.fleet_repair_line.fleet_id.license_plate
+# 		res.x_year_modelo=repair_x.fleet_repair_line.fleet_id.model_year
+# 		res.x_unidad=repair_x.fleet_repair_line.fleet_id.x_numero_unidad
+# 		res.x_kilometraje=repair_x.fleet_repair_line.fleet_id.odometer
+# #		res.x_unidadKilometro=repair_x.fleet_repair_line.fleet_id.odometer_unit
+# 		res.x_vehiculo=repair_x.fleet_repair_line.fleet_id
+# 		return res
 
 	@api.model_create_multi
 	def create(self,vals):
 		print("dentro de unidad")
-		if vals.get('origin'):
-			sale_obj = self.env['sale.order'].search([('name', '=', vals.get('origin'))])
+		res = super(factura, self).create(vals)
+		print(res)
+		print(res.id)
+		print(res.origin)
+		if res.origin:
 
-			print(sale_obj.fleet_repair_id.fleet_id.id)
-			print(sale_obj)
-			print(sale_obj.name)
-			res = super(editable_factura, self).create(vals)
-			print(res)
-			print(res.id)
-			repair_x=self.env['fleet.repair'].search([('id','=',sale_obj.fleet_repair_id.id)],limit=1)
+			sale_order=self.env['sale.order'].search([('name','=',res.origin)],limit=1)
+			repair_x=self.env['fleet.repair'].search([('id','=',sale_order.fleet_repair_id.id)],limit=1)
 			print(repair_x)
 			print(repair_x.fleet_repair_line.fleet_id.model_id)
 			#res.write({'x_modelo':[(4,repair_x.fleet_repair_line.fleet_id.model_id)]})
@@ -51,9 +58,10 @@ class editable_factura(models.Model):
 			res.x_marca=repair_x.fleet_repair_line.fleet_id.brand_id
 			res.x_Matricula=repair_x.fleet_repair_line.fleet_id.license_plate
 			res.x_year_modelo=repair_x.fleet_repair_line.fleet_id.model_year
-			res.x_unidad=repair_x.fleet_repair_line.fleet_id.x_numero_unidad
-			
+			res.x_kilometraje=repair_x.fleet_repair_line.fleet_id.odometer
+			res.x_unidadKilometro=repair_x.fleet_repair_line.fleet_id.odometer_unit
 			res.x_vehiculo=repair_x.fleet_repair_line.fleet_id
+
 		return res
 
 
